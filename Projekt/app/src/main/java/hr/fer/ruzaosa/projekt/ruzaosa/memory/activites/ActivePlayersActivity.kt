627ccf7a3b80335eeb31b53fa6562485c1bc5ca2
Lucija <lucija.aleksic@fer.hr>
2021-01-07 19:06:42 +0100

    Dodatak simuliranja ukljucivanja u igru sa odgovarajucim funkcionalnostima

--- Projekt/app/src/main/java/hr/fer/ruzaosa/projekt/ruzaosa/memory/activites/ActivePlayersActivity.kt
+++ Projekt/app/src/main/java/hr/fer/ruzaosa/projekt/ruzaosa/memory/activites/ActivePlayersActivity.kt
@@ -1,11 +1,10 @@
 package hr.fer.ruzaosa.projekt.ruzaosa.memory.activites;
 
-import android.content.Context
-import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+import android.content.Intent
+import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import hr.fer.ruzaosa.lecture4.ruzaosa.R
 import kotlinx.android.synthetic.main.activity_active_players.*
 
@@ -14,22 +13,48 @@ public class ActivePlayersActivity : AppCompatActivity() {
 //    var prefs: SharedPreferences = applicationContext.getSharedPreferences("UserPrefs",
 //                                    Context.MODE_PRIVATE)
 
-    lateinit var activePlayersList: MutableList<String>
+    lateinit var activePlayers: MutableList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_active_players)
 
-//        activePlayersList = ArrayList<String>()
+          activePlayers = ArrayList<String>()
 //        var username = prefs.getString("Username", "")
 //        activePlayersList.add(username!!)
-//        val adapter: ArrayAdapter<String?> = ArrayAdapter<String?>(
-//                this@ActivePlayersActivity,
-//                android.R.layout.simple_list_item_1,
-//                activePlayersList as List<String?>
-//        )
-//        activePlayersListView.adapter = adapter
+        activePlayers.add("username1")
+        activePlayers.add("username2")
+        activePlayers.add("username3")
+        activePlayers.add("username4")
+        activePlayers.add("username5")
+        activePlayers.add("username6")
+        activePlayers.add("username7")
+        activePlayers.add("username8")
+        activePlayers.add("username9")
+        activePlayers.add("username10")
+        activePlayers.add("username11")
+        activePlayers.add("username12")
+        activePlayers.add("username13")
+        activePlayers.add("username14")
+        activePlayers.add("username15")
 
-        exitActivePlayers.setOnClickListener{ finish() }
+        val itemsAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, activePlayers)
+        activePlayersList.adapter = itemsAdapter
+
+        activePlayersList.onItemClickListener =
+                AdapterView.OnItemClickListener { adapterView, view, i, l ->
+                    val value: String = adapterView.getItemAtPosition(i) as String
+                    val intent = Intent(this, ChallengeActivity::class.java)
+                    intent.putExtra("challengedUser", value)
+                    intent.putExtra("role", "host")
+                    startActivity(intent)
+                }
+        simulBtn.setOnClickListener {
+            val intent = Intent(this, ChallengeActivity::class.java)
+            intent.putExtra("challengedUser", "me")
+            intent.putExtra("role", "guest")
+            startActivity(intent)
+        }
+        exitActivePlayers.setOnClickListener { finish() }
     }
 }
---
+++ Projekt/app/src/main/java/hr/fer/ruzaosa/projekt/ruzaosa/memory/activites/ChallengeActivity.kt
@@ -0,0 +1,44 @@
+package hr.fer.ruzaosa.projekt.ruzaosa.memory.activites
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import hr.fer.ruzaosa.lecture4.ruzaosa.R
+import hr.fer.ruzaosa.lecture4.ruzaosa.k.activites.MenuActivity
+import kotlinx.android.synthetic.main.activity_challenge.*
+
+class ChallengeActivity : AppCompatActivity() {
+    @SuppressLint("ResourceAsColor")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_challenge)
+
+        val challengedUser = intent.extras?.getString("challengedUser")
+        val role = intent.extras?.getString("role")
+
+        when (role) {
+            "host" -> {
+                usernameText.setText(challengedUser)
+                buttonReject.visibility = View.INVISIBLE
+                buttonAccept.visibility = View.INVISIBLE
+                textView.setText("awaiting response...")
+            }
+            "guest" -> {
+                usernameText.setText("Random user")
+                textView.setText("challenged you!")
+                progressBar2.visibility = View.INVISIBLE
+                buttonAccept.setOnClickListener {
+                    val myIntent = Intent(this@ChallengeActivity, GameActivity::class.java)
+                    this@ChallengeActivity.startActivity(myIntent)
+                }
+                buttonReject.setOnClickListener {
+                    finish()
+                }
+            }
+        }
+
+
+    }
+}
