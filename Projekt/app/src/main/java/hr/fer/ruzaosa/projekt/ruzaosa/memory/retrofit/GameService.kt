e9a98bdfdda801c5e557892e85bef4af651cff22
Ana Marija <ana-marija.pavicic@fer.hr>
2021-01-10 13:53:09 +0100

    Dodan GameService.kt, prebačane neke funncije iz UsersService.kt

---
+++ Projekt/app/src/main/java/hr/fer/ruzaosa/projekt/ruzaosa/memory/retrofit/GameService.kt
@@ -0,0 +1,26 @@
+package hr.fer.ruzaosa.projekt.ruzaosa.memory.retrofit
+
+import hr.fer.ruzaosa.lecture4.ruzaosa.k.retrofit.User
+import okhttp3.ResponseBody
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface GameService {
+    @Headers("Content-Type:application/json")
+    @POST("gameAccepted")
+    fun gameAccepted(
+            @Body info: User
+    ): retrofit2.Call<ResponseBody>
+    @Headers("Content-Type:application/json")
+    @POST("gameRejected")
+    fun gameRejected(
+            @Body info: User
+    ): retrofit2.Call<ResponseBody>
+
+    @Headers("Content-Type:application/json")
+    @POST("createGame")
+    fun createGame(
+            @Body info: GameBody
+    ): retrofit2.Call<ResponseBody>
+}
