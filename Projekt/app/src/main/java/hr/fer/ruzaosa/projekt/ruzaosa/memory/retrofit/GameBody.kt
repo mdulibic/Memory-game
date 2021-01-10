74255cf705c290ad0d3239c8c934c4725f7efe56
Ana Marija <ana-marija.pavicic@fer.hr>
2021-01-10 13:59:47 +0100

    Sitne promjene

--- Projekt/app/src/main/java/hr/fer/ruzaosa/projekt/ruzaosa/memory/retrofit/GameBody.kt
+++ Projekt/app/src/main/java/hr/fer/ruzaosa/projekt/ruzaosa/memory/retrofit/GameBody.kt
@@ -2,6 +2,6 @@ package hr.fer.ruzaosa.projekt.ruzaosa.memory.retrofit
 
 import hr.fer.ruzaosa.lecture4.ruzaosa.k.retrofit.User
 
-data class GameBody(val user1: User, val user2: User, val gameId: Long) {
+data class GameBody(val challenger: User, val challenged: User, val gameId: Long) {
     // je li potreban LocalDateTime1 / 2 ?
 }
