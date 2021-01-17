97ebd4f0df6f57aa786d327b19114cff891a03c6
Elena <elena.wachtler@fer.hr>
2021-01-17 10:54:48 +0100

    rješenje završetka igre - slanje Toasta pobjedniku(challenger/challenged) i PUSH notifikacije gubitniku:( (challenger/challenged)

--- Projekt/app/src/main/java/hr/fer/ruzaosa/projekt/ruzaosa/memory/retrofit/GameBody.kt
+++ Projekt/app/src/main/java/hr/fer/ruzaosa/projekt/ruzaosa/memory/retrofit/GameBody.kt
@@ -3,6 +3,6 @@ package hr.fer.ruzaosa.projekt.ruzaosa.memory.retrofit
 import hr.fer.ruzaosa.lecture4.ruzaosa.k.retrofit.User
 import java.io.Serializable
 
-data class GameBody(val challenger: User, val challenged: User): Serializable {
+data class GameBody(val challenger: User, val challenged: User, val gameId: Long): Serializable {
 
 }
